743cae5315845f88fb9ce2f21621d097467ac397
mohitgehi <mohitgehi23@gmail.com>
2020-10-21 20:21:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [text, setText] = React.useState("");
   return (
     <div id="main">
       // Do not alter the main div
+      <button
+        id="click"
+        onClick={() => {
+          setText(
+            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          );
+        }}
+      >
+        <p id="para">{text}</p>
+      </button>
     </div>
   );
 }
-
 
 export default App;
