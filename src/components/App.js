11e3596a15fed670ed1e832beadf6f78d6897411
Mohit Gehi <47024623+mohitgehi@users.noreply.github.com>
2020-10-21 21:06:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,21 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [text, setText] = React.useState("");
+  const [text, setText] = React.useState([]);
+  const addText = () => {
+    setText(
+      text.concat(
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
-      <button
-        id="click"
-        onClick={() => {
-          setText(
-            "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-          );
-        }}
-      >
-       
-      </button>
-      <p id="para">{text}</p>
+      <button id="click" onClick={addText}></button>
+      {text}
     </div>
   );
 }
